92ec1a25f32b5169c934b5ed16e9ff30175ed5ed
Simone <info@simoneicardi.com>
2025-09-29 12:08:37 +0200

    accordion base

--- src/components/accordion/AccordionItem.jsx
+++ src/components/accordion/AccordionItem.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
+
 export default function AccordionItem(props) {
 
+    // descrutturazione delle props
     const { titolo, contenuto } = props;
+
+    // variabile di stato di apertura accordion
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="accordion">
             <div className="accordion__title">
                 <h3>{titolo}</h3>
-                <button>+</button>
+                <button
+                    className="accordion__btn"
+                    onClick={() => setIsOpen(currentValue => !currentValue)}
+                >
+                    {isOpen ? "-" : "+"}
+                </button>
             </div>
-            <div>
-                {contenuto}
-            </div>
+            {isOpen && <div>{contenuto}</div>}
         </div>
     );
 }
