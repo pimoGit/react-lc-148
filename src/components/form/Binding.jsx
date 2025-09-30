13573c23a4b5b1f177d211b352ebf018ceb074fb
Simone <info@simoneicardi.com>
2025-09-30 10:52:06 +0200

    aggiungi form e preventDefault

--- src/components/form/Binding.jsx
+++ src/components/form/Binding.jsx
@@ -3,14 +3,21 @@ import { useState } from "react";
 export default function Binding() {
     const [firstName, setFirstName] = useState('');
 
+    // funzione di invio form
+    const handleSubmit = e => {
+        e.preventDefault();
+        alert('FORM INVIATO!')
+    }
+
     return (
-        <>
+        <form onSubmit={handleSubmit}>
             <input
                 type="text"
                 value={firstName}
                 onChange={e => { setFirstName(e.target.value) }}
             />
             <p>Il tuo nome è {firstName}</p>
-        </>
+            <button>Invia</button>
+        </form>
     )
 }
