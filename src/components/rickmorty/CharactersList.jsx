33a1f225293d6dd487ff9e39f5cfbdfd5ab2a395
Simone <info@simoneicardi.com>
2025-10-02 12:29:30 +0200

    aggiungi jsx completo con info personaggi

--- src/components/rickmorty/CharactersList.jsx
+++ src/components/rickmorty/CharactersList.jsx
@@ -19,12 +19,24 @@ export default function CharactersList() {
         , [])
 
     return (
-        <div>
-            <ul>
+        <div className="container">
+            <h1>Rick & Morty - Wiki</h1>
+            <div className="characters">
                 {characters.map((character) => (
-                    <li key={character.id}>{character.name}</li>
+                    <div className="character" key={character.id}>
+                        <div className="character-image">
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div className="character-info">
+                            <p className="character-name">{character.name}</p>
+                            <p className="character-status-origin">
+                                <span className={`status-icon is-${character.status.toLowerCase()}`}></span>
+                                {character.status} - {character.origin.name}
+                            </p>
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
