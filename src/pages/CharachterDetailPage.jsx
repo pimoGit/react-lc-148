df91ee20d57c45d9bb3c7e179502537930dc933a
Simone <info@simoneicardi.com>
2025-10-07 12:45:45 +0200

    page di dettaglio con chiamata completa

--- src/pages/CharachterDetailPage.jsx
+++ src/pages/CharachterDetailPage.jsx
@@ -1,17 +1,50 @@
+// import stato e effect
+import { useState, useEffect } from "react";
+// import di comp LInk da libreria router
+import { Link } from "react-router-dom";
 // import dell'hook per il parametro
 import { useParams } from "react-router-dom";
+
 
 
 const CharachterDetailPage = () => {
     // recuperiamo il parametro dinamico (id)
     const { id } = useParams();
 
+    // stato del componente (oggetto del personaggio da API)
+    const [character, setCharacter] = useState();
+
+    // a mounting comp parte chiamata a API
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+            .then(res => setCharacter(res.data))
+            .catch(err => console.log(err)
+            )
+    }, [])
+
     return (
-        <>
-            <p>Ciao sono la pagina di dettaglio</p>
-            <h2>ID prodotto: {id}</h2>
-        </>
-    )
+        <div>
+            {character ? (
+                <div className="character" >
+                    <div className="character-image">
+                        <img src={character.image} alt={character.name} />
+                    </div>
+                    <div className="character-info">
+                        <p className="character-name">{character.name}</p>
+                        <p className="character-status-origin">
+                            <span className={`status-icon is-${character.status.toLowerCase()}`}></span>
+                            {character.status} - {character.origin.name}
+                        </p>
+                        <Link to={`/charachters`}>
+                            Listato personaggi
+                        </Link>
+                    </div>
+                </div>
+            )
+                :
+                (<p>Loading...</p>)}
+        </div>)
+
 }
 
 export default CharachterDetailPage
